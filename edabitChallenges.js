21d586d6a5beeb5796c5e41331b1a4ef604aade0
Mary Brady <emceebrady@gmail.com>
2020-03-28 11:24:53 -0600

    very easy challenges

---
+++ edabitChallenges.js
@@ -0,0 +1,12 @@
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+function divisibleByFive(n) {
+    return (n % 5 === 0);
+}
+
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function lessThan100(a, b) {
+    return ((a + b) < 100);
+}
+
